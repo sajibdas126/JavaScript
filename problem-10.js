cfa316d3b899d215daca702cdde6b09e65970c93
Sajib <sajibdashtrg@gmail.com>
2025-10-07 18:32:09 +0600

    function use kora vowel and consonant check

--- problem-10.js
+++ problem-10.js
@@ -1,11 +1,27 @@
 // Problem 10: Check Vowel or Consonant
 
-let ch ='i';
+// let ch ='i';
 
-ch = ch.toLocaleLowerCase();
+// ch = ch.toLocaleLowerCase();
 
-if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-    console.log("Vowel");
-}else {
-    console.log("Consonant");
+// if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+//     console.log("Vowel");
+// }else {
+//     console.log("Consonant");
+// }
+
+const Vowel = ["a","e","i","o","u"]
+
+function countVowels(sentence){
+    let count = 0;
+    const letters = Array.from(sentence);
+
+    letters.forEach(function(Value){
+        if(Vowel.includes(Value)){
+            count++;
+        }
+    })
+    return count;
 }
+
+console.log(countVowels("i love bangledesh"))
